00a812f7b345e75ce4c0bb62618b8e745a61fa22
to-fuu <chebbi.ala@outlook.com>
2022-04-25 20:17:13 +0100

    home page

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/demo1',
-        permanent: false,
-      },
-    ]
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/',
+  //       destination: '/demo1',
+  //       permanent: false,
+  //     },
+  //   ]
+  // },
 }
 
 module.exports = nextConfig
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,18 @@
+import Link from "next/link"
 
 
 export default function Home() {
 
-  return <></>
+  return <main className="grid min-h-screen font-bold uppercase bg-neutral-900 text-neutral-200 font-Demo1 place-items-center" >
+
+    <ul className="flex flex-col items-center justify-center h-full gap-10 text-[min(7vw,72px)]">
+      <li>
+        <Link href={'/demo1'}><a className="hover:underline underline-offset-8 hover:text-[#e1ff00]">Demo 1</a></Link>
+      </li>
+      <li>
+        <Link href={'/demo2'}><a className="hover:underline underline-offset-8 hover:text-[#db5d4f]">Demo 2</a></Link>
+      </li>
+    </ul>
+
+  </main >
 }
